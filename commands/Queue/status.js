bb5d8d5d3632fa4efd215685875577f591a29493
FREAK-HEAD <abulfa12345@gmail.com>
2021-09-19 10:23:38 +0530

    first commit

---
+++ commands/Queue/status.js
@@ -0,0 +1,105 @@
+const {
+	MessageEmbed,
+	Message
+} = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+const settings = require("../../botconfig/settings.json");
+const {
+	check_if_dj
+} = require("../../handlers/functions")
+module.exports = {
+	name: "status", //the command name for the Slash Command
+
+	category: "Queue",
+	aliases: ["stats"],
+	usage: "status",
+
+	description: "Shows the Queue Status", //the command description for Slash Command Overview
+	cooldown: 10,
+	requiredroles: [], //Only allow specific Users with a Role to execute a Command [OPTIONAL]
+	alloweduserids: [], //Only allow specific Users to execute a Command [OPTIONAL]
+	run: async (client, message, args) => {
+		try {
+			//things u can directly access in an interaction!
+			const {
+				member,
+				channelId,
+				guildId,
+				applicationId,
+				commandName,
+				deferred,
+				replied,
+				ephemeral,
+				options,
+				id,
+				createdTimestamp
+			} = message;
+			const {
+				guild
+			} = member;
+			const {
+				channel
+			} = member.voice;
+			if (!channel) return message.reply({
+				embeds: [
+					new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **Please join ${guild.me.voice.channel ? "__my__" : "a"} VoiceChannel First!**`)
+				],
+
+			})
+			if (channel.guild.me.voice.channel && channel.guild.me.voice.channel.id != channel.id) {
+				return message.reply({
+					embeds: [new MessageEmbed()
+						.setColor(ee.wrongcolor)
+						.setFooter(ee.footertext, ee.footericon)
+						.setTitle(`${client.allEmojis.x} Join __my__ Voice Channel!`)
+						.setDescription(`<#${guild.me.voice.channel.id}>`)
+					],
+				});
+			}
+			try {
+				let newQueue = client.distube.getQueue(guildId);
+				if (!newQueue || !newQueue.songs || newQueue.songs.length == 0) return message.reply({
+					embeds: [
+						new MessageEmbed().setColor(ee.wrongcolor).setTitle(`${client.allEmojis.x} **I am not playing anything right now!**`)
+					],
+
+				})
+				var djs = client.settings.get(newQueue.id, `djroles`).map(r => `<@&${r}>`);
+				if (djs.length == 0) djs = "`not setup`";
+				else djs.join(", ");
+				let newTrack = newQueue.songs[0];
+				let embed = new MessageEmbed().setColor(ee.color)
+					.setDescription(`See the [Queue on the **DASHBOARD** Live!](https://Musicium-2.freakhead007.repl.co/queue/${newQueue.id})`)
+					.addField(`💡 Requested by:`, `>>> ${newTrack.user}`, true)
+					.addField(`⏱ Duration:`, `>>> \`${newQueue.formattedCurrentTime} / ${newTrack.formattedDuration}\``, true)
+					.addField(`🌀 Queue:`, `>>> \`${newQueue.songs.length} song(s)\`\n\`${newQueue.formattedDuration}\``, true)
+					.addField(`🔊 Volume:`, `>>> \`${newQueue.volume} %\``, true)
+					.addField(`♾ Loop:`, `>>> ${newQueue.repeatMode ? newQueue.repeatMode === 2 ? `${client.allEmojis.check_mark} \`Queue\`` : `${client.allEmojis.check_mark} \`Song\`` : `${client.allEmojis.x}`}`, true)
+					.addField(`↪️ Autoplay:`, `>>> ${newQueue.autoplay ? `${client.allEmojis.check_mark}` : `${client.allEmojis.x}`}`, true)
+					.addField(`❔ Filter${newQueue.filters.length > 0 ? "s": ""}:`, `>>> ${newQueue.filters && newQueue.filters.length > 0 ? `${newQueue.filters.map(f=>`\`${f}\``).join(`, `)}` : `${client.allEmojis.x}`}`, newQueue.filters.length > 1 ? false : true)
+					.addField(`🎧 DJ-Role${client.settings.get(newQueue.id, "djroles").length > 1 ? "s": ""}:`, `>>> ${djs}`, client.settings.get(newQueue.id, "djroles").length > 1 ? false : true)
+					.setAuthor(`${newTrack.name}`, `https://cdn.discordapp.com/attachments/856114693739773952/873890345518235648/PicsArt_08-06-06.25.14.jpg`, newTrack.url)
+					.setThumbnail(`https://img.youtube.com/vi/${newTrack.id}/mqdefault.jpg`)
+					.setFooter(` ${newTrack.user.tag}`, newTrack.user.displayAvatarURL({
+						dynamic: true
+					}));
+				message.reply({
+					embeds: [embed]
+				})
+			} catch (e) {
+				console.log(e.stack ? e.stack : e)
+				message.reply({
+					content: `${client.allEmojis.x} | Error: `,
+					embeds: [
+						new MessageEmbed().setColor(ee.wrongcolor)
+						.setDescription(`\`\`\`${e}\`\`\``)
+					],
+
+				})
+			}
+		} catch (e) {
+			console.log(String(e.stack).bgRed)
+		}
+	}
+}
